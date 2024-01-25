c6c97474922c5e51fc53b0f2851ced3a185b0dc3
hans <hanskrupakar1994@gmail.com>
2024-01-25 15:30:11 +0530

    added-bahdanau-attention-lstm-models

---
+++ pytorch/morphologically_rich_MT/models/lstm.py
@@ -0,0 +1,106 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+MAX_LENGTH, SOS_token = 20, 0
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, dropout_p=0.1):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers=3, batch_first=True)
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, input):
+        embedded = self.dropout(self.embedding(input))
+        output, hidden = self.lstm(embedded)
+        return output, hidden
+
+class BahdanauAttention(nn.Module):
+    def __init__(self, hidden_size):
+        super(BahdanauAttention, self).__init__()
+        self.Wa = nn.Linear(hidden_size, hidden_size)
+        self.Ua = nn.Linear(hidden_size, hidden_size)
+        self.Va = nn.Linear(hidden_size, 1)
+
+    def forward(self, query, keys):
+        query = query[0] + query[1] # h_n + c_n (h_n alone is better)
+        scores = self.Va(torch.tanh(self.Wa(query) + self.Ua(keys)))
+        scores = scores.squeeze(2).unsqueeze(1)
+
+        weights = F.softmax(scores, dim=-1)
+        context = torch.bmm(weights, keys)
+
+        return context, weights
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1):
+        super(AttnDecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.attention = BahdanauAttention(hidden_size)
+        self.lstm = nn.LSTM(2 * hidden_size, hidden_size, num_layers=3, batch_first=True)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=device).fill_(SOS_token)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+        attentions = []
+
+        for i in range(MAX_LENGTH):
+            decoder_output, decoder_hidden, attn_weights = self.forward_step(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_outputs.append(decoder_output)
+            attentions.append(attn_weights)
+
+            if target_tensor is not None:
+                # Teacher forcing: Feed the target as the next input
+                decoder_input = target_tensor[:, i].unsqueeze(1) # Teacher forcing
+            else:
+                # Without teacher forcing: use its own predictions as the next input
+                _, topi = decoder_output.topk(1)
+                decoder_input = topi.squeeze(-1).detach()  # detach from history as input
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
+        attentions = torch.cat(attentions, dim=1)
+
+        return decoder_outputs, decoder_hidden, attentions
+
+
+    def forward_step(self, input, hidden, encoder_outputs):
+        embedded =  self.dropout(self.embedding(input))
+
+        query = [torch.sum(x.permute(1, 0, 2), axis=1).unsqueeze(1) for x in hidden]
+        context, attn_weights = self.attention(query, encoder_outputs)
+
+        input_gru = torch.cat((embedded, context), dim=2)
+        
+        output, hidden = self.lstm(input_gru, hidden)
+        output = self.out(output)
+
+        return output, hidden, attn_weights
+
+if __name__ == "__main__":
+
+    word2vec_vector_size = 100
+    hidden_size = 128
+    bucket = [30,20]
+    device = torch.device("cuda")
+
+    encoder = EncoderRNN(word2vec_vector_size, hidden_size).to(device)
+    decoder = AttnDecoderRNN(hidden_size, word2vec_vector_size).to(device)
+    
+    input_tensor = torch.ones((64,bucket[0])).long().to(device)
+    target_tensor = torch.ones((64,bucket[1])).long().to(device)
+    encoder_outputs, encoder_hidden = encoder(input_tensor)
+    print ('encoder outputs', encoder_outputs.shape,'encoder hidden',  [x.shape for x in encoder_hidden])
+    #print ([x.shape for x in encoder_outputs], [x.shape for x in encoder_hidden])
+
+    decoder_outputs, decoder_hidden, attn = decoder(encoder_outputs, encoder_hidden, target_tensor)
+    print (decoder_outputs.shape, [x.shape for x in decoder_hidden], attn.shape)
