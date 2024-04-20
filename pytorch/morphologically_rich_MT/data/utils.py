cf431ce5c7478cf1cde113b7e1a45472601f4a85
hans <hanskrupakar1994@gmail.com>
2024-04-20 11:21:17 +0530

    bucketing-sampling-without-replacement

--- pytorch/morphologically_rich_MT/data/utils.py
+++ pytorch/morphologically_rich_MT/data/utils.py
@@ -13,6 +13,30 @@ def get_sentences_from_file(l1_path, l2_path):
     
     return l1_sentences, l2_sentences
 
+class BucketingBatchSamplerReplace(Sampler):
+    def __init__(self, bucketing_indices, batch_size):
+        self.bucketing_indices = bucketing_indices
+        self.batch_size = batch_size
+
+        self.batches = []
+        for b_idx in range(len(bucketing_indices)):
+            st, en = bucketing_indices[b_idx]
+
+            if en - st < batch_size:
+                self.batches.append((st, en))
+            else:
+                bucket_batches = [(x, x+batch_size) for x in range(st, en-batch_size+1)]
+                self.batches.extend(bucket_batches)
+        
+        self.sampler = torch.randperm(len(self.batches))
+
+    def __len__(self) -> int:
+        return len(self.batches)
+
+    def __iter__(self):
+        for sample_idx in self.sampler:
+            yield range(self.batches[sample_idx][0], self.batches[sample_idx][1])
+
 #TODO: Batch sequence from lowest to biggest bucket
 class BucketingBatchSampler(Sampler):
     def __init__(self, bucketing_indices, batch_size):
@@ -28,11 +52,7 @@ class BucketingBatchSampler(Sampler):
             start, end = self.bucketing_indices[bucket_sample]
 
             if end + 1 - start < self.batch_size:
-                print (start, end)
-                print (list(range(start, end+1)))
                 yield range(start, end+1)
             else:
                 start_idx = torch.randint(low=start, high=end+1-self.batch_size, size=(1,))
-                print (start, end)
-                print (list(range(start_idx, start_idx+self.batch_size)))
                 yield range(start_idx, start_idx+self.batch_size)
